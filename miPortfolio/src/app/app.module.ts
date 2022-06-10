290ded48ea67fe9f314a80d41c4afd27b2d42686
Pablo Lupo <pablolupo84@gmail.com>
2022-06-10 13:37:28 -0300

    se agrega portada y se cambian estilos en general

--- miPortfolio/src/app/app.module.ts
+++ miPortfolio/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PortadaComponent } from './components/portada/portada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PortadaComponent
   ],
   imports: [
     BrowserModule
